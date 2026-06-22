de696875c69306c80b099051a6bfe916b30f2c10
tala <talakamel@gmail.com>
2026-06-22 21:17:22 +0200

    Test

---
+++ src/core/ot_auction.py
@@ -0,0 +1,130 @@
+import numpy as np
+
+class AuctionOT:
+    def __init__(self, cost_matrix, mu_X, mu_Y, epsilon=None):
+        self.C = np.array(cost_matrix, dtype=float)
+        self.mu_X = np.array(mu_X, dtype=int)
+        self.mu_Y = np.array(mu_Y, dtype=int)
+        self.N_X, self.N_Y = self.C.shape
+        
+        assert np.sum(self.mu_X) == np.sum(self.mu_Y), "Total mass in X must equal total mass in Y"
+        
+        if epsilon is None:
+            self.epsilon = 1.0 / (np.sum(self.mu_X) + 1.0)
+        else:
+            self.epsilon = epsilon
+            
+        self.mu = np.zeros((self.N_X, self.N_Y), dtype=int) # Coupling matrix
+        
+        self.y_atoms = []
+        for y in range(self.N_Y):
+            self.y_atoms.append([{'x': -1, 'mass': self.mu_Y[y], 'beta': 0.0}])
+
+    def solve(self):
+        iterations = 0
+        while True:
+            # Calculate unassigned mass for each x
+            assigned_X = np.sum(self.mu, axis=1)
+            unassigned_X = self.mu_X - assigned_X
+            
+            # If all mass is assigned, we are done
+            if np.sum(unassigned_X) == 0:
+                break
+                
+            bids = self._bidding_phase(unassigned_X)
+            self._assignment_phase(bids)
+            iterations += 1
+            
+        total_cost = np.sum(self.mu * self.C)
+        return self.mu, total_cost, iterations
+
+    def _bidding_phase(self, unassigned_X):
+        bids = {y: [] for y in range(self.N_Y)}
+        
+        for x in range(self.N_X):
+            mass_to_assign = unassigned_X[x]
+            if mass_to_assign <= 0:
+                continue
+                
+            # Construct the Pi(x) list (Eq. 10 & 11)
+            # Contains tuples: (effective_cost, target_y, mass_available, occupied_by_x, current_beta)
+            Pi_x = []
+            
+            for y in range(self.N_Y):
+                for atom in self.y_atoms[y]:
+                    x_prime = atom['x']
+                    # Do not bid against our own mass atoms to prevent inefficient competition
+                    if x_prime == x:
+                        continue 
+                    
+                    eff_cost = self.C[x, y] - atom['beta']
+                    Pi_x.append((eff_cost, y, atom['mass'], x_prime, atom['beta']))
+            
+            # Sort Pi(x) in ascending order (Eq. 11)
+            Pi_x.sort(key=lambda item: item[0])
+            
+            # Determine bid recipients and the m-th entry for alpha' (Eq. 12)
+            accumulated_mass = 0
+            bid_targets = []
+            alpha_prime = 0
+            
+            for i, item in enumerate(Pi_x):
+                eff_cost, y, mass_avail, x_prime, old_beta = item
+                
+                # We want to claim this mass
+                claim_amount = min(mass_avail, mass_to_assign - accumulated_mass)
+                if claim_amount > 0:
+                    bid_targets.append({'y': y, 'mass': claim_amount, 'eff_cost': eff_cost, 'x_prime': x_prime})
+                    accumulated_mass += claim_amount
+                
+                # Once we have found enough mass to fulfill our needs, the *next* worst price sets alpha'
+                if accumulated_mass >= mass_to_assign:
+                    # Eq 12: alpha'(x) is determined by the next entry in Pi_x
+                    if i + 1 < len(Pi_x):
+                        alpha_prime = Pi_x[i+1][0]
+                    else:
+                        alpha_prime = eff_cost # Fallback if we hit the exact end of the list
+                    break
+            
+            # Submit the bids
+            for target in bid_targets:
+                y = target['y']
+                # Equivalent to Eq 8, adjusted for generalized alpha_prime
+                bid_value = self.C[x, y] - alpha_prime - self.epsilon
+                bids[y].append({'x': x, 'mass': target['mass'], 'bid_value': bid_value, 'x_prime': target['x_prime']})
+                
+        return bids
+
+    def _assignment_phase(self, bids):
+        for y, y_bids in bids.items():
+            if not y_bids:
+                continue
+                
+            # Process bids for this y. To keep it faithful, lowest bid value gets accepted.
+            y_bids.sort(key=lambda b: b['bid_value'])
+            
+            for bid in y_bids:
+                x = bid['x']
+                mass_won = bid['mass']
+                new_beta = bid['bid_value']
+                x_prime = bid['x_prime']
+                
+                # Find the atom in y we are trying to displace
+                for i, atom in enumerate(self.y_atoms[y]):
+                    if atom['x'] == x_prime and atom['mass'] >= mass_won:
+                        # Remove old assignment from coupling matrix
+                        if x_prime != -1:
+                            self.mu[x_prime, y] -= mass_won
+                            
+                        # Add new assignment
+                        self.mu[x, y] += mass_won
+                        
+                        # Update the y_atoms tracking
+                        atom['mass'] -= mass_won
+                        
+                        # Insert the newly won mass atom
+                        self.y_atoms[y].append({'x': x, 'mass': mass_won, 'beta': new_beta})
+                        break
+                        
+            # Cleanup zero-mass atoms in y
+            self.y_atoms[y] = [atom for atom in self.y_atoms[y] if atom['mass'] > 0]
