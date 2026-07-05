2027e6f38c1a3fa2bfc77d80d09972fd513b9268
cobra-web <talakamel@gmail.com>
2026-07-05 21:04:20 +0200

    Refactor SparseNeighborhood to use numpy arrays

--- src/utils/data_structures.py
+++ src/utils/data_structures.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 class SparseNeighborhood:
     def __init__(self, n_X, n_Y, initial_edges=None):
         self.n_X = n_X
         self.n_Y = n_Y
         
-        self.adj_X = {x: set() for x in range(n_X)}
+        self.adj_X = [np.array([], dtype=np.int32) for _ in range(n_X)]
         self.edge_count = 0
         
         if initial_edges is not None:
@@ -11,12 +13,23 @@ class SparseNeighborhood:
 
     def add_edge(self, x, y):
         if y not in self.adj_X[x]:
-            self.adj_X[x].add(y)
+            self.adj_X[x] = np.append(self.adj_X[x], y)
             self.edge_count += 1
 
     def add_edges(self, edges):
-        for x, y in edges:
-            self.add_edge(x, y)
+        edges = np.asarray(edges, dtype=np.int32)
+        if len(edges) == 0:
+            return
+
+        unique_x = np.unique(edges[:, 0])
+        for x in unique_x:
+            new_y = edges[edges[:, 0] == x, 1]
+            
+            combined = np.concatenate((self.adj_X[x], new_y))
+            unique_combined = np.unique(combined)
+            
+            self.edge_count += (len(unique_combined) - len(self.adj_X[x]))
+            self.adj_X[x] = unique_combined
 
     def get_allowed_y(self, x):
         return self.adj_X[x]
@@ -25,11 +38,13 @@ class SparseNeighborhood:
         return y in self.adj_X[x]
 
     def get_all_edges(self):
-        edges = []
-        for x, y_set in self.adj_X.items():
-            for y in y_set:
-                edges.append((x, y))
-        return edges
+        if self.edge_count == 0:
+            return np.empty((0, 2), dtype=np.int32)
+            
+        x_indices = np.repeat(np.arange(self.n_X), [len(y_arr) for y_arr in self.adj_X])
+        y_indices = np.concatenate(self.adj_X)
+        
+        return np.column_stack((x_indices, y_indices))
 
     def sparsity_ratio(self):
         max_edges = self.n_X * self.n_Y
